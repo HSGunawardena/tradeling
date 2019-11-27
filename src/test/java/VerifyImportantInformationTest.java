2554693b4abd0faeb39827baa6b61cccde379f21
Saliya Gunawardena <saliyaguna@gmail.com>
2019-11-27 23:48:30 +0530

    Uncomment the tearDown method
    Signed-off-by: Saliya Gunawardena <saliyaguna@gmail.com>

--- src/test/java/VerifyImportantInformationTest.java
+++ src/test/java/VerifyImportantInformationTest.java
@@ -28,6 +28,6 @@ public class VerifyImportantInformationTest {
 
     @AfterMethod
     public void tearDown() {
-//        Tradeling.getInstance().closeBrowser();
+        Tradeling.getInstance().closeBrowser();
     }
 }
